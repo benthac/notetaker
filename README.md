1fb9256014873def35e8ad91f1739d3584fda8ff
ben thacker <benthac@gmail.com>
2021-07-15 16:40:16 -0400

    finished routes and moved files into the public folder

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Note Taker Starter Code
+
+# As a user i can create a new note, and add it to a column
+
+# the application is deployed to heroku, as well as the 3001 port
+
+[Click the link](http://localhost:3001/)
+
+![myImage](https://nameless-ocean-78672.herokuapp.com/)
--- server.js
+++ server.js
@@ -1,23 +1,43 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const db = require('./db.json');
+const { v1: uuidv1 } = require('uuid');
 //const { notes } = require('./data/notes');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get('/notes', (req, res ) => {
-    res.sendFile(path.join(__dirname, './data/notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
   });
 
 app.get('/', (req, res ) => {
-    res.sendFile(path.join(__dirname, './data/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
   });
   
 app.get('/api/notes', (req, res) => {
-  
+    res.json(db);
   });
   
-app.post('/api/notes', (req, res) => {});
+app.post('/api/notes', (req, res) => {
+  const { title, text } = req.body
+  const newNote = {
+    title, 
+    text,
+    id: uuidv1()
+  }
+    db.push(newNote);
+    fs.writeFile(path.join(__dirname, './db.json'), JSON.stringify(db), (err) => {
+      console.log(err);
+      })
+  res.json(newNote)
+});
   
 app.listen(PORT, () => {
     console.log(`now on port ${PORT}`)
